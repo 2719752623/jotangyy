5d9d0a7b6689d5b8f25b0c1085c9db183ec4daf8
2719752623 <2719752623@feishu.uestc.cn>
2023-10-24 07:52:48 +0800

    Add files for task #4 and #6

---
+++ My-Project/src/test/java/com/example/My/Project/MyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.My.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+@TableName("article")
+public class Article {
+}
